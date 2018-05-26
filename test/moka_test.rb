f3a0ee774e40658925e21633e54406f936db06ef
Farhad Eyvazli <farhad9801@gmail.com>
2018-05-26 18:12:33 +0000

    updated

--- test/moka_test.rb
+++ test/moka_test.rb
@@ -15,7 +15,7 @@ module Moka
     def test_should_get_moka_informations_and_return_check_key
       assert_equal Moka.config.dealer_code, "123456"
       assert_equal Moka.config.username, "ZXCVBNVBN"
-      assert_equal Moka.config.password, "YHSUSHDYHUDHD"
+      assert_equal Moka.config.password, "abcdef"
       assert_equal Moka.config.check_key, "ff4a6ee22aeafe87f7930f84b5ce2ad9655bfc6b5e430644a88c5b75a09fdee1"
     end
   end
