2f3c02be732e86a57d85be5c833143f28eb74e4c
Farhad <farhad9801@gmail.com>
2018-06-28 15:53:33 +0400

    test

--- test/moka_test.rb
+++ test/moka_test.rb
@@ -6,16 +6,16 @@ module Moka
     include Minitest::Hooks
     def setup
       Moka.configure env: :test do |config|
-        config.dealer_code = "1730"
-        config.username = "TestKorhan"
-        config.password = "YHSUSHDYHUDHD"
+        config.dealer_code = ENV['MOKA_DEALER_CODE']
+        config.username = ENV['MOKA_USERNAME']
+        config.password = ENV['MOKA_PASSWORD']
       end
     end
 
     def test_should_get_moka_informations_and_return_check_key
-      assert_equal Moka.config.dealer_code, "1730"
-      assert_equal Moka.config.username, "TestKorhan"
-      assert_equal Moka.config.password, "YHSUSHDYHUDHD"
+      assert_equal Moka.config.dealer_code, ENV['MOKA_DEALER_CODE']
+      assert_equal Moka.config.username, ENV['MOKA_USERNAME']
+      assert_equal Moka.config.password, ENV['MOKA_PASSWORD']
       #assert_equal Moka.config.check_key, "ff4a6ee22aeafe87f7930f84b5ce2ad9655bfc6b5e430644a88c5b75a09fdee1"
     end
   end
